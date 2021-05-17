5c1944801e744d0be2ef0947767cdba5aaf99931
ML-Josh <joshua.bj@medialand.tw>
2021-05-17 14:16:05 +0800

    Add residential upload router

--- server/app/router/v1.0/upload.js
+++ server/app/router/v1.0/upload.js
@@ -4,5 +4,6 @@ const uploadController = require('~server/app/controller/v1.0/uploadController')
 const router = express.Router();
 
 router.get('/commercial', uploadController.commercial);
+router.get('/residential', uploadController.residential);
 
 module.exports = router;
